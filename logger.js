6d70ff141ac9af3846fc82d26991fcf0bd2530ad
Chris <christian.j.kesler@gmail.com>
2022-10-07 11:29:46 -0700

    Improve logger

--- logger.js
+++ logger.js
@@ -1,49 +1,116 @@
+// HOW TO USE
+// ================================================================
+
+// ROUTE
+// var logger = require('./utils/logger.js')
+// 
+// app.get('/', function (req, res) {
+//   logger.compliantUser(req)
+//   res.render('index')
+// })
+
+// ================================================================
+
 var fs = require('fs');
 
 var logger = {
-  maliciousUser: function (ip, requested, datetime) {
+  maliciousUser: function (req) {
+    var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null;
+    let datetime = new Date();
+
+    fs.appendFile(
+      __dirname + '/../../model/logs/index.txt',
+      '================' +
+      '\n\tDATETIME -> ' +
+      datetime +
+      '\n\tIP -> ' +
+      ip +
+      '\n\treq.url -> ' +
+      req.url +
+      '\n================',
+      function (err) {
+        if (err) throw err;
+      }
+    );
+
     fs.appendFile(
       __dirname + '/../../model/logs/maliciousUsers.txt',
       '================' +
-        '\n\tDATETIME -> ' +
-        datetime +
-        '\n\tIP -> ' +
-        ip +
-        '\n\tREQUESTED -> ' +
-        requested +
-        '\n================',
+      '\n\tDATETIME -> ' +
+      datetime +
+      '\n\tIP -> ' +
+      ip +
+      '\n\treq.url -> ' +
+      req.url +
+      '\n================',
       function (err) {
         if (err) throw err;
       }
     );
   },
-  compliantUser: function (ip, requested, datetime) {
+  compliantUser: function (req) {
+    var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null;
+    let datetime = new Date();
+
+    fs.appendFile(
+      __dirname + '/../../model/logs/index.txt',
+      '================' +
+      '\n\tDATETIME -> ' +
+      datetime +
+      '\n\tIP -> ' +
+      ip +
+      '\n\treq.url -> ' +
+      req.url +
+      '\n================',
+      function (err) {
+        if (err) throw err;
+      }
+    );
+
     fs.appendFile(
       __dirname + '/../../model/logs/compliantUsers.txt',
       '================' +
-        '\n\tDATETIME -> ' +
-        datetime +
-        '\n\tIP -> ' +
-        ip +
-        '\n\tREQUESTED -> ' +
-        requested +
-        '\n================',
+      '\n\tDATETIME -> ' +
+      datetime +
+      '\n\tIP -> ' +
+      ip +
+      '\n\treq.url -> ' +
+      req.url +
+      '\n================',
       function (err) {
         if (err) throw err;
       }
     );
   },
-  lostUser: function (ip, requested, datetime) {
+  lostUser: function (req) {
+    var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null;
+    let datetime = new Date();
+
+    fs.appendFile(
+      __dirname + '/../../model/logs/index.txt',
+      '================' +
+      '\n\tDATETIME -> ' +
+      datetime +
+      '\n\tIP -> ' +
+      ip +
+      '\n\treq.url -> ' +
+      req.url +
+      '\n================',
+      function (err) {
+        if (err) throw err;
+      }
+    );
+
     fs.appendFile(
       __dirname + '/../../model/logs/lostUsers.txt',
       '================' +
-        '\n\tDATETIME -> ' +
-        datetime +
-        '\n\tIP -> ' +
-        ip +
-        '\n\tREQUESTED -> ' +
-        requested +
-        '\n================',
+      '\n\tDATETIME -> ' +
+      datetime +
+      '\n\tIP -> ' +
+      ip +
+      '\n\treq.url -> ' +
+      req.url +
+      '\n================',
       function (err) {
         if (err) throw err;
       }
